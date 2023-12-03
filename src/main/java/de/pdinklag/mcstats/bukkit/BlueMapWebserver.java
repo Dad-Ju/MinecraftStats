5f3cb6261085e7fe30dd0b591d832651a0b8c94f
Juu <justin.bedrich@gmail.com>
2023-12-04 00:43:42 +0100

    add api support, this will propebly do nothing...

--- src/main/java/de/pdinklag/mcstats/bukkit/BlueMapWebserver.java
+++ src/main/java/de/pdinklag/mcstats/bukkit/BlueMapWebserver.java
@@ -1,31 +1,28 @@
 package de.pdinklag.mcstats.bukkit;
 
 import java.nio.file.Path;
+import java.util.Optional;
 
-import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.Plugin;
+
+import de.bluecolored.bluemap.api.BlueMapAPI;
 
 public class BlueMapWebserver extends PluginWebserver {
     public static final String PLUGIN_NAME = "BlueMap";
-    
-    private static final String CONFIG_FILENAME = "webserver.conf";
+
+    // private static final String CONFIG_FILENAME = "webserver.conf";
     private static final String DEFAULT_WEBROOT = "bluemap/web";
 
     private final Path documentRoot;
 
     public BlueMapWebserver(Plugin bluemapPlugin) {
-        final Path bluemapPath = bluemapPlugin.getDataFolder().toPath().toAbsolutePath();
-        
-        String webRoot;
-        try {
-            final YamlConfiguration bluemapWebserverConfig = YamlConfiguration.loadConfiguration(bluemapPath.resolve(CONFIG_FILENAME).toFile());
-            webRoot = bluemapWebserverConfig.getString("webroot", DEFAULT_WEBROOT);
-        } catch(Exception e) {
-            e.printStackTrace();
-            webRoot = DEFAULT_WEBROOT;
-        }
+        Optional<BlueMapAPI> optionalApi = BlueMapAPI.getInstance();
 
-        this.documentRoot = bluemapPlugin.getServer().getWorldContainer().toPath().resolve(webRoot).toAbsolutePath();
+        if (optionalApi.isPresent()) {
+            this.documentRoot = optionalApi.get().getWebApp().getWebRoot();
+        } else {
+            this.documentRoot = Path.of(DEFAULT_WEBROOT);
+        }
     }
 
     @Override
